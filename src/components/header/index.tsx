da0b5c23f339df60324f7ef91e184b4c01277241
SaiMaheshwarReddy <maheshwarreddysai@gmail.com>
2026-09-13 15:20:44 +0530

    increased logo size

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const Header = () => {
   return (
     <header className="py-4 flex items-center justify-between">
-      <Image src="/logo.png" alt="Logo" width={100} height={100} />
+      <Image src="/logo.png" alt="Logo" width={140} height={140} />
       <Avatar>
         {/* <AvatarImage src="" alt="@shadcn" className="grayscale" /> */}
         <AvatarFallback>S</AvatarFallback>
